87d7ffaad96f25be1a69ddc79065f12dc1692147
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-12-13 23:48:12 +0700

    Add basic functionality for WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anh Nguyen on 13/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
