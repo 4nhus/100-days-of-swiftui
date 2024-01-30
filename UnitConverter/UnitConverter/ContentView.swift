b0780cecdd0a5a428c4ad7a61a49c3484ae7eac5
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-30 11:32:37 +1100

    Initialise new project for unit converter app

---
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by Anh Nguyen on 30/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Anh Nguyen on 30/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
