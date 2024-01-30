1ce0d50db3a23291bd4cfb4280534f9dd4c05127
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-30 18:06:44 +1100

    Initialise new project for BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anh Nguyen on 30/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
