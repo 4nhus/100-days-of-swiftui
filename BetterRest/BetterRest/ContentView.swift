4be949ec88235843b60b4c61ef4a9889d6db1e11
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-30 18:51:45 +1100

    Replace daily coffee intake Stepper with Picker

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -36,7 +36,11 @@ struct ContentView: View {
                 }
                 
                 Section("Daily coffee intake") {
-                    Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                    Picker("Number of cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text($0, format: .number)
+                        }
+                    }
                 }
             }
             .navigationTitle("BetterRest")
