12fdce32a9a1e56e8e2d4f142e17b26a6ffc02ed
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-30 12:09:14 +1100

    Add length conversion

--- UnitConverter/UnitConverter/ContentView.swift
+++ UnitConverter/UnitConverter/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let units: [UnitLength] = [UnitLength.millimeters, UnitLength.centimeters, UnitLength.meters, UnitLength.kilometers, UnitLength.feet, UnitLength.yards, UnitLength.miles]
+    
+    @State private var inputUnit: UnitLength = UnitLength.millimeters
+    @State private var outputUnit: UnitLength = UnitLength.millimeters
+    @State private var valueToConvert: Double = 0
+    @FocusState private var valueToConvertIsFocused: Bool
+    
+    var convertedValue: Double {
+        Measurement(value: valueToConvert, unit: inputUnit).converted(to: outputUnit).value
+    }
+    
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Select your input unit") {
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Select your output unit") {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section("Enter your value to convert") {
+                    TextField("Value to convert", value: $valueToConvert, format: .number)
+                        .keyboardType(.numberPad)
+                        .focused($valueToConvertIsFocused)
+                    
+                }
+                Section("Your converted value is") {
+                    Text("\(convertedValue.formatted() + outputUnit.symbol)")
+                }
+            }
+            .navigationTitle("Length Converter")
+            .toolbar {
+                if valueToConvertIsFocused {
+                    Button("Done") {
+                        valueToConvertIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
