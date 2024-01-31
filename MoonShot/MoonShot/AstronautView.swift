04c20eef2a6928e8e5bef882927c5f1eeb4fb6f3
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-31 16:33:44 +1100

    Add basic functionality for MoonShot

---
+++ MoonShot/MoonShot/AstronautView.swift
@@ -0,0 +1,36 @@
+//
+//  AstronautView.swift
+//  MoonShot
+//
+//  Created by Anh Nguyen on 31/1/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
