24c081b8bf1a1b2fd85886d6cca3029b74104658
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-31 17:28:12 +1100

    Initialise new project for Navigation

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Anh Nguyen on 31/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
