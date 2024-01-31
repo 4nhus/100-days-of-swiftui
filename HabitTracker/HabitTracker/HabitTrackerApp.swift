6eb523725097048af4960a2f931f472f9d637315
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-31 21:29:02 +1100

    Initialise new project for HabitTracker

---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Anh Nguyen on 31/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
