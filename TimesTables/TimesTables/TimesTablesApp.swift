5f76b3b6bbbaeafd944886d9403ef99b4d57b349
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-31 11:30:05 +1100

    Initialise new project for TimesTables

---
+++ TimesTables/TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Anh Nguyen on 31/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
