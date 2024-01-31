752d43d15106938aadd6e876c1e6b699322e0395
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-31 18:06:49 +1100

    Replace TextField for expense item name with editable inline NavigationTitle

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AddView: View {
     var expenses: Expenses
     
-    @State private var name = ""
+    @State private var name = "Default expense name"
     @State private var type = "Personal"
     @State private var amount = 0.0
     
@@ -21,8 +21,6 @@ struct AddView: View {
     var body: some View {
         NavigationStack {
             Form {
-                TextField("Name", text: $name)
-
                 Picker("Type", selection: $type) {
                     ForEach(types, id: \.self) {
                         Text($0)
@@ -32,7 +30,8 @@ struct AddView: View {
                 TextField("Amount", value: $amount, format: .currency(code:  Locale.current.currency?.identifier ?? "USD"))
                     .keyboardType(.decimalPad)
             }
-            .navigationTitle("Add new expense")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle($name)
             .toolbar {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
