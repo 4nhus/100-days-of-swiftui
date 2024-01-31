c089013d6a55f221ee785bda67b165a653abf72f
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-31 14:00:26 +1100

    Initialise new project for iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Anh Nguyen on 31/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
