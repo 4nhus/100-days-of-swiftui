e370960d8ffa354ac31fc2d919701bbe282866c2
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-01-31 15:11:56 +1100

    Initialise new project for MoonShot

---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Anh Nguyen on 31/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
