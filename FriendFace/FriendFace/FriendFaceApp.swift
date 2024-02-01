816a289541b260390169cbd9225a56ddfce32f5e
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-01 20:22:53 +1100

    Initialise new project for FriendFace

---
+++ FriendFace/FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Anh Nguyen on 1/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
