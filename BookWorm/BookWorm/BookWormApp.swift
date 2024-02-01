46c76588164cf7958f62c072db9dcac239d0d9b1
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-01 14:03:59 +1100

    Initialise new project for BookWorm

---
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Anh Nguyen on 1/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
