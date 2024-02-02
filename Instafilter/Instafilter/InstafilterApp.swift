5bf1686ce61cfda32f6942eccc748c77510f4471
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-02 11:45:56 +1100

    Initialise new project for Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Anh Nguyen on 2/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
