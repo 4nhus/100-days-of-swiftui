06507937aac67edb9f5fc12b69bfa0674eea2232
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-02 20:10:11 +1100

    Initialise new project for FaceTagger

---
+++ FaceTagger/FaceTagger/FaceTaggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceTaggerApp.swift
+//  FaceTagger
+//
+//  Created by Anh Nguyen on 2/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceTaggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
