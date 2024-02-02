bc52b54635db3f77af98dce2067e264f813a07db
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-02 12:34:48 +1100

    Initialise new project for BucketList

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Anh Nguyen on 2/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
