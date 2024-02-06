137e4a7904cbbb9e4e30aa7fbdb734d52e02e5a1
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-07 09:39:49 +1100

    Initialise new project for LayoutAndGeometry

---
+++ LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Anh Nguyen on 7/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
