dba5462befb3807abec17d288f915b4158ef11bb
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-06 23:22:13 +1100

    Add basic functionality for Flashzilla

---
+++ Flashzilla/Flashzilla/CardView.swift
@@ -0,0 +1,86 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Anh Nguyen on 6/2/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
+    
+    let card: Card
+    
+    var removal: (() -> Void)? = nil
+    
+    @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(
+                    differentiateWithoutColor
+                        ? .white
+                        : .white
+                            .opacity(1 - Double(abs(offset.width / 50)))
+
+                )
+                .background(
+                    differentiateWithoutColor
+                        ? nil
+                        : RoundedRectangle(cornerRadius: 25, style: .continuous)
+                            .fill(offset.width > 0 ? .green : .red)
+                )
+                .shadow(radius: 10)
+            
+            VStack {
+                if voiceOverEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .animation(.spring(), value: offset)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .accessibilityAddTraits(.isButton)
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
+    }
+}
+
+#Preview {
+    CardView(card: Card.example)
+}
--- Flashzilla/Flashzilla/FlashzillaApp.swift
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 @main
+
 struct FlashzillaApp: App {
     var body: some Scene {
         WindowGroup {
