a951c7c4d6559b3ef6d826bd6245222876da9ba0
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-06 12:49:01 +1100

    Initialise new project for Flashzilla

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Anh Nguyen on 6/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
