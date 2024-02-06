ff3c5668337e0a5fc1977d5e217f53fd7da003a5
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-07 10:00:58 +1100

    Add gradual fade out to 0 opacity, starting 200 points from top of ContentView

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
-
+    
     var body: some View {
         GeometryReader { fullView in
             ScrollView(.vertical) {
@@ -20,6 +20,7 @@ struct ContentView: View {
                             .frame(maxWidth: .infinity)
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(geo.frame(in: .global).minY / 200)
                     }
                     .frame(height: 40)
                 }
