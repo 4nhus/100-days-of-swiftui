a394c67b382b535ad06c493a177aaab9d1c1e3f8
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-07 15:35:03 +1100

    Add flicking through values on dice roll

--- HighRollers/HighRollers/RollView.swift
+++ HighRollers/HighRollers/RollView.swift
@@ -5,6 +5,7 @@
 //  Created by Anh Nguyen on 7/2/2024.
 //
 
+import Combine
 import SwiftData
 import SwiftUI
 
@@ -23,18 +24,27 @@ struct RollView: View {
     @Environment(\.dismiss) private var dismiss
     @Query private var results: [Result]
     
+    @State var timer = Timer.publish(every: 1000, on: .main, in: .common).autoconnect()
     @State private var dice = Dice.four
     @State private var isShowingChangeDice = false
     @State private var rollStartFlag = false
     @State private var rollFinishFlag = false
+    @State private var flicksRemaining = 5
+    @State private var result: Int?
     
     var body: some View {
         NavigationStack {
             Button("Roll \(dice.rawValue) sided dice") {
                 rollStartFlag.toggle()
-                modelContext.insert(Result(value: dice.roll(), diceSides: dice.rawValue, dateAdded: Date.now))
+                timer = Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()
                 rollFinishFlag.toggle()
             }
+            .animation(.spring, value: result)
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            Text(result != nil ? "\(result!)" : "")
+                .font(.largeTitle)
+                .accessibilityHidden(flicksRemaining == 0)
             .toolbar {
                 Menu("Change dice") {
                     Picker("Dice", selection: $dice) {
@@ -47,6 +57,22 @@ struct RollView: View {
             .navigationTitle("Roll")
             .sensoryFeedback(.start, trigger: rollStartFlag)
             .sensoryFeedback(.success, trigger: rollFinishFlag)
+            .onReceive(timer) { time in
+                if flicksRemaining > 0 {
+                    result = dice.roll()
+                    flicksRemaining -= 1
+                } else {
+                    timer.upstream.connect().cancel()
+                    modelContext.insert(Result(value: result!, diceSides: dice.rawValue, dateAdded: Date.now))
+                    flicksRemaining = 5
+                }
+            }
         }
     }
+}
+
+
+#Preview {
+    RollView()
+        .modelContainer(for: Result.self)
 }
