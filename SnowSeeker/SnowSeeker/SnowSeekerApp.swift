498cb5bcb868b23051319601dc9ef8b1fdf51bd7
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-07 15:40:52 +1100

    Initialise new project for SnowSeeker

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Anh Nguyen on 7/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
