e382fad05c8a46d0d9eab43438aa5b4db3269b17
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-07 17:03:35 +1100

    Add loading and saving Favourites to documentsDirectory

--- SnowSeeker/SnowSeeker/Favorites.swift
+++ SnowSeeker/SnowSeeker/Favorites.swift
@@ -7,40 +7,45 @@
 
 import SwiftUI
 
-class Favorites: ObservableObject {
+class Favorites: ObservableObject, Codable {
     // the actual resorts the user has favorited
     private var resorts: Set<String>
-
-    // the key we're using to read/write in UserDefaults
-    private let saveKey = "Favorites"
-
+    
+    static let url = URL.documentsDirectory.appending(path: "favorites.json")
+    
     init() {
-        // load our saved data
-
-        // still here? Use an empty array
-        resorts = []
+        do {
+            let data = try Data(contentsOf: Favorites.url)
+            let decodedFavorites = try JSONDecoder().decode(Favorites.self, from: data)
+            self.resorts = decodedFavorites.resorts
+        }
+        catch {
+            self.resorts = []
+        }
     }
-
+    
     // returns true if our set contains this resort
     func contains(_ resort: Resort) -> Bool {
         resorts.contains(resort.id)
     }
-
+    
     // adds the resort to our set, updates all views, and saves the change
     func add(_ resort: Resort) {
         objectWillChange.send()
         resorts.insert(resort.id)
         save()
     }
-
+    
     // removes the resort from our set, updates all views, and saves the change
     func remove(_ resort: Resort) {
         objectWillChange.send()
         resorts.remove(resort.id)
         save()
     }
-
+    
     func save() {
-        // write out our data
+        if let data = try? JSONEncoder().encode(self) {
+            try? data.write(to: Favorites.url)
+        }
     }
 }
