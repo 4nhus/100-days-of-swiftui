0c9a0e293db05bcc6559793d546afb4852deb137
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-07 11:22:44 +1100

    Initialise new project for HighRollers

---
+++ HighRollers/HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Anh Nguyen on 7/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
